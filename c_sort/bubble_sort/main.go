a4b4bfd1a3cffff39174815cc142a955b3a3f168
xx19941215 <xx19931215@gmail.com>
2019-03-26 19:46:07 +0800

    update

--- c_sort/bubble_sort/main.go
+++ c_sort/bubble_sort/main.go
@@ -5,21 +5,30 @@ import (
 )
 
 func main() {
-	arr := [5]int{12, 34, 3, 4, 45}
+	arr := [9]int{12, 34, 3, 4, 45, 100, 34, 88, 76}
 	bubbleSort(&arr)
 	fmt.Println(arr)
 }
 
-func bubbleSort(arr *[5]int) {
+func bubbleSort(arr *[9]int) {
 	temp := 0
+	len := len(arr)
 
-	for i := 0; i < len(*arr) - 1; i++ {
-		for j := 0; j < len(*arr) - 1 - i; j++ {
-			if (*arr)[j + 1] < (*arr)[j] {
-				temp = (*arr)[j]
-				(*arr)[j] = (*arr)[j + 1]
-				(*arr)[j + 1] = temp
+	//外层进行len-1次循环
+	for i := 0; i < len-1; i++ {
+		swapped := false
+		//内循环比较len-i-i次
+		for j := 0; j < len-1-i; j++ {
+			if arr[j+1] < arr[j] {
+				temp = arr[j]
+				arr[j] = arr[j+1]
+				arr[j+1] = temp
+				swapped = true
 			}
+		}
+		//没有发生交换，说明有序 直接结束算法
+		if swapped == false {
+			break
 		}
 	}
 }
